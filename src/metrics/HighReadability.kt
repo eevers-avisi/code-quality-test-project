58d70fb652def903badefd952dfefe7c216c7982
Jordi Martijn van Os <j.m.os@avisi.nl>
2020-10-28 14:16:33 +0100

    Test

--- src/metrics/HighReadability.kt
+++ src/metrics/HighReadability.kt
@@ -7,7 +7,7 @@
  * @return ArrayList containing *limit* digits of the fibonacci sequence, with a minimum size of 3: (0, 1, 1)
  */
 fun fibonacciSequenceUpTo(limit: Int): ArrayList<Int> {
-    val foo = arrayListOf<Int>(0, 1, 2)
+    val foo = arrayListOf<Int>(0, 1, 3)
     var index = 1
     // -2 As the index starts two positions behind the number that is calculated in the while loop
     while (index < limit - 2) {
