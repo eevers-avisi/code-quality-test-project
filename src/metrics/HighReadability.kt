0b007b244f73b1f5b01ff5e4bf942142b8800d4f
Jordi Martijn van Os <j.m.os@avisi.nl>
2020-10-29 11:38:00 +0100

    W

--- src/metrics/HighReadability.kt
+++ src/metrics/HighReadability.kt
@@ -1,6 +1,6 @@
 
 /**
- * Callculates and returns the fibonacci sequence
+ * Calculates and returns the fibonacci sequence
  * See https://en.wikipedia.org/wiki/Fibonacci_number for more information regarding the fibonacci sequence
  *
  *
@@ -8,11 +8,11 @@
  * @return ArrayList containing *limit* digits of the fibonacci sequence, with a minimum size of 3: (0, 1, 1)
  */
 fun fibonacciSequenceUpTo(limit: Int): ArrayList<Int> {
-    val fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable = arrayListOf<Int>(0, 1, 1)
+    val onlesbarelangesequentievantroep = arrayListOf<Int>(0, 1, 1)
     var index = 1
     // -2 As the index starts two positions behind the number that is calculated in the while loop
     while (index < limit - 2) {
-        fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable.add(fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable[index++] + fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable[index])
+        onlesbarelangesequentievantroep.add(onlesbarelangesequentievantroep[index++] + onlesbarelangesequentievantroep[index])
     }
-    return fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable
+    return onlesbarelangesequentievantroep
 }
