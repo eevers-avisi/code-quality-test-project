4d9ae6f56d06768b77f04b44ba00411a235841d4
Jordi Martijn van Os <j.m.os@avisi.nl>
2020-10-29 08:56:49 +0100

    Test

--- src/metrics/HighReadability.kt
+++ src/metrics/HighReadability.kt
@@ -7,11 +7,11 @@
  * @return ArrayList containing *limit* digits of the fibonacci sequence, with a minimum size of 3: (0, 1, 1)
  */
 fun fibonacciSequenceUpTo(limit: Int): ArrayList<Int> {
-    val fibonacciSequence = arrayListOf<Int>(0, 1, 1)
+    val fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable = arrayListOf<Int>(0, 1, 1)
     var index = 1
     // -2 As the index starts two positions behind the number that is calculated in the while loop
     while (index < limit - 2) {
-        fibonacciSequence.add(fibonacciSequence[index++] + fibonacciSequence[index])
+        fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable.add(fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable[index++] + fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable[index])
     }
-    return fibonacciSequence
+    return fibonacciSeqToAnEvenLongerNameThatIsQuiteUnreadable
 }
